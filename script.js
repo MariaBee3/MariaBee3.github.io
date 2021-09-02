220567858609fd1e5e0fbe81e8c6ee1c0708db47
Maria Besteman <maria.besteman@gmail.com>
2021-09-02 11:06:24 +0200

    add portfolio page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Skills homepage - alt text appears on hovering logos//
 const img = document.querySelectorAll('.skills'); 
 let skillText = document.querySelector('.skilltext');
 
@@ -14,3 +16,23 @@ img.forEach (element => {
     skillText.innerHTML = ''
     }); 
 });
+
+//Portfolio page - collapsible//
+
+function readMore() {
+    let dots = document.querySelector('.dots')
+    let moreText = document.querySelector('.more'); 
+    let btnText = document.querySelector('.collBtn'); 
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline"; 
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none"; 
+    } else {
+        dots.style.display = "none"; 
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline"; 
+    }
+};
+
+

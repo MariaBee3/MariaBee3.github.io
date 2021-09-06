1c46bd005ec04984c9a97a175516538011405506
Maria Besteman <maria.besteman@gmail.com>
2021-09-06 10:26:32 +0200

    fix toggle button text feature collapsible portfolio

--- script.js
+++ script.js
@@ -19,21 +19,6 @@ img.forEach (element => {
 
 //Portfolio page - collapsible//
 
-/*
-var btn = document.querySelectorAll(".collBtn");
-   
-        btn[0].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var content = this.previousElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block"; 
-                this.innerHTML = "Read less"; 
-            }
-        });
-*/
-
 var btn = document.querySelectorAll(".collBtn");
    
         btn.forEach(button => {
@@ -42,44 +27,12 @@ var btn = document.querySelectorAll(".collBtn");
             var content = button.previousElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
+                button.innerHTML = "Read more"
             } else {
                 content.style.display = "block"; 
                 button.innerHTML = "Read less"; 
             }
         })
     }); 
-
-
-/*
-document.querySelectorAll('.collBtn').forEach(button => {
-    button.addEventListener("click", function () {
-        button.classList.toggle("active");
-        var content = button.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block"
-            btn.innerHTML = "Read less";
-        }
-    });
-    })
-})
-*/  
-
-/*Deze code is ook optie om panel naar beneden te laten schuiven. Werkte echter nog niet. 
-document.querySelectorAll('.collBtn').forEach(button => {
-    button.addEventListener('click', () => {
-        const panel = button.nextElementSibling; 
-
-        button.classList.toggle('collBtn--active'); 
-
-        if (button.classList.contains('collBtn--active')) {
-            panel.getElementsByClassName.maxHeight = panel.scrollHeight + 'px'; 
-        } else {
-            panel.getElementsByClassName.maxHeight = 0; 
-        }
-    })
-})
-*/
 
 

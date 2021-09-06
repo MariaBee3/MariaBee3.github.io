535cd7f9f6ef36871a29a2aa6ab905f4085efd0f
Maria Besteman <maria.besteman@gmail.com>
2021-09-06 10:15:38 +0200

    add collapsible content to portfolio

--- script.js
+++ script.js
@@ -19,20 +19,67 @@ img.forEach (element => {
 
 //Portfolio page - collapsible//
 
-function readMore() {
-    let dots = document.querySelector('.dots')
-    let moreText = document.querySelector('.more'); 
-    let btnText = document.querySelector('.collBtn'); 
+/*
+var btn = document.querySelectorAll(".collBtn");
+   
+        btn[0].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.previousElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block"; 
+                this.innerHTML = "Read less"; 
+            }
+        });
+*/
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline"; 
-        btnText.innerHTML = "Read more"; 
-        moreText.style.display = "none"; 
-    } else {
-        dots.style.display = "none"; 
-        btnText.innerHTML = "Read less"; 
-        moreText.style.display = "inline"; 
-    }
-};
+var btn = document.querySelectorAll(".collBtn");
+   
+        btn.forEach(button => {
+            button.addEventListener("click", function () {
+            button.classList.toggle("active");
+            var content = button.previousElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block"; 
+                button.innerHTML = "Read less"; 
+            }
+        })
+    }); 
+
+
+/*
+document.querySelectorAll('.collBtn').forEach(button => {
+    button.addEventListener("click", function () {
+        button.classList.toggle("active");
+        var content = button.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block"
+            btn.innerHTML = "Read less";
+        }
+    });
+    })
+})
+*/  
+
+/*Deze code is ook optie om panel naar beneden te laten schuiven. Werkte echter nog niet. 
+document.querySelectorAll('.collBtn').forEach(button => {
+    button.addEventListener('click', () => {
+        const panel = button.nextElementSibling; 
+
+        button.classList.toggle('collBtn--active'); 
+
+        if (button.classList.contains('collBtn--active')) {
+            panel.getElementsByClassName.maxHeight = panel.scrollHeight + 'px'; 
+        } else {
+            panel.getElementsByClassName.maxHeight = 0; 
+        }
+    })
+})
+*/
 
 
